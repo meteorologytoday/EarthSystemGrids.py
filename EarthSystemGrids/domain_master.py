58b5de7f4b3cbf35e083c128fb69db917cc04db1
Tien-Yiao Hsu <tienyiao@pik-potsdam.de>
2026-08-19 16:38:47 +0200

    Crafting domain master

---
+++ EarthSystemGrids/domain_master.py
@@ -0,0 +1,37 @@
+from dataclass import dataclass
+
+# So to convert
+
+@dataclass
+class Domain:
+    grid:
+    landsea_mask: 
+
+@dataclass
+class Transformation:
+    grid_pair: list[str]
+    method: str
+
+
+
+class DomainMaster:
+    
+    def __init__(self):
+
+
+
+    def register_domain(
+        self,
+        domain:str,
+        grid:str,
+        landsea_mask:str,
+    ) -> None:
+
+    def transform_scalar(
+        method,
+    ) -> Array:
+
+
+    def trans
+
+
